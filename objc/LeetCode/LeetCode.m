acaa628a7adf1732ea5ab5a26c4ff35c9d5afc35
Jason Prasad <jasongprasad@gmail.com>
2020-05-20 11:41:00 -0400

    Updates comments and adds start of contains duplicate

--- objc/LeetCode/LeetCode.m
+++ objc/LeetCode/LeetCode.m
@@ -17,13 +17,18 @@
 
 // MARK: - 1. Two Sum
 
-/// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-/// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-///
-/// Given nums = [2, 7, 11, 15], target = 9,
-///
-/// Because nums[0] + nums[1] = 2 + 7 = 9,
-/// return [0, 1].
+/**
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ Example:
+
+ Given nums = [2, 7, 11, 15], target = 9,
+
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ */
 - (NSArray<NSNumber *> *)twoSumNumbers:(NSArray<NSNumber *> *)nums andTarget:(NSNumber *)target {
     NSMutableDictionary *numsDict = [[NSMutableDictionary alloc] init];
     for (int i = 0; i < nums.count; ++i) {
@@ -45,20 +50,18 @@
 
 // MARK: - 238. Product of Array Except Self
 
-/// Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product
-/// of all the elements of nums except nums[i].
-///
-/// Input:  [1,2,3,4]
-/// Output: [24,12,8,6]
-///
-/// Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the
-/// whole array) fits in a 32 bit integer.
-///
-/// Note: Please solve it without division and in O(n).
-///
-/// Follow up:
-/// Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose
-/// of space complexity analysis.)
+/**
+ Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+ Example:
+ Input:  [1,2,3,4]
+ Output: [24,12,8,6]
+
+ Note: Please solve it without division and in O(n).
+
+ Follow up:
+ Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+ */
 - (NSArray<NSNumber *> *)productExceptSelf:(NSArray<NSNumber *> *)nums {
     NSMutableArray<NSNumber *> *result = [nums mutableCopy];
     result[0] = @1;
@@ -83,31 +86,35 @@
 
 // MARK: - 20. Valid Parentheses
 
-/// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-/// An input string is valid if:
-///   1. Open brackets must be closed by the same type of brackets.
-///   2. Open brackets must be closed in the correct order.
-/// Note that an empty string is also considered valid.
-///
-/// Example 1:
-///   Input: "()"
-///   Output: true
-///
-/// Example 2:
-///   Input: "()[]{}"
-///   Output: true
-///
-/// Example 3:
-///   Input: "(]"
-///   Output: false
-///
-/// Example 4:
-///   Input: "([)]"
-///   Output: false
-///
-/// Example 5:
-///   Input: "{[]}"
-///   Output: true
+/**
+ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+ An input string is valid if:
+
+ Open brackets must be closed by the same type of brackets.
+ Open brackets must be closed in the correct order.
+ Note that an empty string is also considered valid.
+
+ Example 1:
+ Input: "()"
+ Output: true
+
+ Example 2:
+ Input: "()[]{}"
+ Output: true
+
+ Example 3:
+ Input: "(]"
+ Output: false
+
+ Example 4:
+ Input: "([)]"
+ Output: false
+
+ Example 5:
+ Input: "{[]}"
+ Output: true
+ */
 - (bool)isValid:(NSString *)s {
     // See https://stackoverflow.com/a/23397279
     NSUInteger len = [s length];
@@ -142,6 +149,95 @@
     XCTAssertFalse([self isValid:@"(]"]);
     XCTAssertFalse([self isValid:@"([)]"]);
     XCTAssertTrue([self isValid:@"{[]}"]);
+}
+
+// MARK: - 242. Valid Anagram
+
+/**
+ Given two strings s and t , write a function to determine if t is an anagram of s.
+
+ Example 1:
+ Input: s = "anagram", t = "nagaram"
+ Output: true
+
+ Example 2:
+ Input: s = "rat", t = "car"
+ Output: false
+
+ Note:
+ You may assume the string contains only lowercase alphabets.
+
+ Follow up:
+ What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ */
+- (BOOL)isAnagramString:(NSString *)s otherString:(NSString *)t {
+    if (s.length != t.length) {
+        return NO;
+    }
+
+    NSUInteger len = [s length];
+    unichar bufferS[len + 1];
+    [s getCharacters:bufferS range:NSMakeRange(0, len)];
+
+    unichar bufferT[len + 1];
+    [t getCharacters:bufferT range:NSMakeRange(0, len)];
+
+    int counts[26];
+    for (int i = 0; i < len; ++i) {
+        counts[bufferS[i] - 'a']++;
+        counts[bufferT[i] - 'a']--;
+    }
+
+    for (int i = 0; i < len; ++i) {;
+        if (counts[i] != 0) {
+            return NO;
+        }
+    }
+
+    return YES;
+}
+
+/*
+ Alternatives:
+ - Sort the arrays using [anArray sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+ The solution would be O(nlogn).
+ - Use a hash table instead of a fixed size counter. Imagine allocating a large size array to fit the entire range of
+ unicode characters, which could go up to more than 1 million. A hash table is a more generic solution and could adapt
+ to any range of characters.
+ */
+
+- (void)testIsAnagrameStringOtherString {
+    XCTAssertTrue([self isAnagramString:@"anagram" otherString:@"nagaram"]);
+    XCTAssertFalse([self isAnagramString:@"rat" otherString:@"car"]);
+}
+
+// MARK: - 217. Contains Duplicate
+
+/**
+ Given an array of integers, find if the array contains any duplicates.
+
+ Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+ Example 1:
+ Input: [1,2,3,1]
+ Output: true
+
+ Example 2:
+ Input: [1,2,3,4]
+ Output: false
+
+ Example 3:
+ Input: [1,1,1,3,3,4,3,2,4,2]
+ Output: true
+ */
+- (BOOL)containsDuplicate:(NSArray<NSNumber *> *)nums {
+    return NO;
+}
+
+- (void)testContainsDuplicate {
+    XCTAssertTrue([self containsDuplicate:(@[@1, @2, @3, @1])]);
+    XCTAssertFalse([self containsDuplicate:(@[@1, @2, @3, @4])]);
+    XCTAssertTrue([self containsDuplicate:(@[@1, @1, @1, @3, @3, @4, @3, @2, @4, @2])]);
 }
 
 @end
