b767c4c42a9ce97591c1ed65fb5f2c37cf9b6285
Jason Grant Prasad <jasongprasad@gmail.com>
2020-05-25 15:48:13 +0000

    Addss max subarray problem

--- python/leetcode/array_test.py
+++ python/leetcode/array_test.py
@@ -3,7 +3,7 @@ import unittest
 from typing import List
 
 
-class TestArrays(unittest.TestCase):
+class TestArray(unittest.TestCase):
 
     """
     56. Merge Intervals
@@ -34,6 +34,23 @@ class TestArrays(unittest.TestCase):
             self.merge_intervals([[1, 4], [4, 5]]),
             [[1, 5]]
         )
+
+    """
+    53. Maximum Subarray
+
+    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+    Example:
+    Input: [-2,1,-3,4,-1,2,1,-5,4],
+    Output: 6
+    Explanation: [4,-1,2,1] has the largest sum = 6.
+
+    Follow up:
+    If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+    """
+
+    def max_sub_array(self, nums: List[int]) -> int:
+        pass
 
 
 if __name__ == '__main__':
